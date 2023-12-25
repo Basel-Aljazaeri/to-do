1b9405a6760d17a04f581b5213df7a06c1b24559
Basel-Aljazaeri <mhd.basel.1995@gmail.com>
2023-12-25 09:38:20 +0000

    modified

---
+++ src/persistence/mysql.js
@@ -0,0 +1,124 @@
+const waitPort = require('wait-port');
+const fs = require('fs');
+const mysql = require('mysql2');
+
+let pool;
+
+async function init () {
+    const host = "localhost";
+    const user = "gameOfTheYear";
+    const password = "game2023";
+    const database = "gameof2023";
+
+    await waitPort({
+        host,
+        port: 3306,
+        timeout: 10000,
+        waitForDns: true,
+    });
+
+    pool = mysql.createPool({
+        connectionLimit: 5,
+        host,
+        user,
+        password,
+        database,
+        charset: 'utf8mb4',
+    });
+
+    return new Promise((acc, rej) => {
+        pool.query(
+            'CREATE TABLE IF NOT EXISTS todo_items (id varchar(36), name varchar(255), completed boolean) DEFAULT CHARSET utf8mb4',
+            err => {
+                if (err) return rej(err);
+
+                console.log(`Connected to mysql db at host ${host}`);
+                acc();
+            },
+        );
+    });
+}
+
+async function teardown () {
+    return new Promise((acc, rej) => {
+        pool.end(err => {
+            if (err) rej(err);
+            else acc();
+        });
+    });
+}
+
+async function getItems () {
+    return new Promise((acc, rej) => {
+        pool.query('SELECT * FROM todo_items', (err, rows) => {
+            if (err) return rej(err);
+            acc(
+                rows.map(item =>
+                    Object.assign({}, item, {
+                        completed: item.completed === 1,
+                    }),
+                ),
+            );
+        });
+    });
+}
+
+async function getItem (id) {
+    return new Promise((acc, rej) => {
+        pool.query('SELECT * FROM todo_items WHERE id=?', [id], (err, rows) => {
+            if (err) return rej(err);
+            acc(
+                rows.map(item =>
+                    Object.assign({}, item, {
+                        completed: item.completed === 1,
+                    }),
+                )[0],
+            );
+        });
+    });
+}
+
+async function storeItem (item) {
+    return new Promise((acc, rej) => {
+        pool.query(
+            'INSERT INTO todo_items (id, name, completed) VALUES (?, ?, ?)',
+            [item.id, item.name, item.completed ? 1 : 0],
+            err => {
+                if (err) return rej(err);
+                acc();
+            },
+        );
+    });
+}
+
+async function updateItem (id, item) {
+    return new Promise((acc, rej) => {
+        pool.query(
+            'UPDATE todo_items SET name=?, completed=? WHERE id=?',
+            [item.name, item.completed ? 1 : 0, id],
+            err => {
+                if (err) return rej(err);
+                acc();
+            },
+        );
+    });
+}
+
+async function removeItem (id) {
+    return new Promise((acc, rej) => {
+        pool.query('DELETE FROM todo_items WHERE id = ?', [id], err => {
+            if (err) return rej(err);
+            acc();
+        });
+    });
+}
+
+module.exports = {
+    init,
+    teardown,
+    getItems,
+    getItem,
+    storeItem,
+    updateItem,
+    removeItem,
+};
